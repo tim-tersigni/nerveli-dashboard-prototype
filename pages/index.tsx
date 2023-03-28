3cb72db14a3c9a2e51a4dc4366b5e967e2410c40
Tim Tersigni <timtersigni@pop-os.localdomain>
2023-03-28 14:37:21 -0500

    Remove boilerplate module styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <h1>Hello World</h1>
       </main>
     </>
   )
