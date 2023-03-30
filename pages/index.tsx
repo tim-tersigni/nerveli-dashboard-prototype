38254f4f9137fad821608cc5af0ddc2b3d7719e6
Tim Tersigni <tim.tersigni@gmail.com>
2023-03-30 14:21:15 -0500

    Remove hello world header

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Hello World</h1>
       </main>
     </Layout>
   )
