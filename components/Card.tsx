84ea3ea59750158564993418d97e868f37bc7c19
Tim Tersigni <tim.tersigni@gmail.com>
2023-04-02 11:57:34 -0500

    Minor changes in Card.tsx for typescript functionality

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React, {ReactNode} from "react";
 import styles from "../styles/Home.module.css";
 
-interface CardProps {
+interface Props {
   title: string;
   body: string;
   children?: ReactNode;
 }
 
-function Card({ title, body, children }) {
+function Card({ title, body, children }: Props) {
   return (
     <div className={styles.card}>
       <h2>{title}</h2>
