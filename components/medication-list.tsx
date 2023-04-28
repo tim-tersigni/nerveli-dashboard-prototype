d571d9428d3b42950abdfb6bc09f476bd30d3aeb
Lisa Wu <59784432+litiwu@users.noreply.github.com>
2023-04-28 15:17:10 -0500

    Adding Medication List Component

---
+++ components/medication-list.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from "react";
+import Card from "./card";
+import ProgressBar from "./progress-bar";
+import styles from "./medication-list.module.css";
+
+const medications = [
+  {
+    id: "1",
+    name: "Adderall",
+    dosage: 30,
+    frequency: "twice a day",
+    numTablets: 1,
+    consistency: 75,
+  },
+  {
+    id: "2",
+    name: "Citalopram",
+    dosage: 20,
+    frequency: "daily",
+    numTablets: 1,
+    consistency: 40,
+  },
+];
+
+function MedicationList() {
+  return (
+    <Card title="Medication">
+      <b>Active Medication</b>
+      <ul className={styles.medList}>
+        {medications.map((med) => (
+          <li key={med.id} className={styles.medication}>
+            <div className={styles.medName}> {med.name} </div>
+            &nbsp;{med.dosage} mg &nbsp;
+            <p>
+              {med.numTablets} {" tablet(s) "}
+              {med.frequency}
+            </p>
+            <ProgressBar progress={med.consistency} />
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
+
+export default MedicationList;
