f24d76963ce5c90ae0f9712b1853b761843992f0
anandpapineni <anandpapineni9@gmail.com>
2023-05-05 21:10:18 -0500

    checks if website is being browser rendered

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,13 @@
 import Card from '@/components/card'
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import { useEffect, useState } from "react";
 
 export default function Document() {
+  const [isBrowser, setIsBrowser] = useState(false);
+
+useEffect(() => {
+ setIsBrowser(typeof window !== "undefined");
+}, []);
   return (
     <Html lang="en">
       <Head />
